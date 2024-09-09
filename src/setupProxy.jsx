154c99e0f5b305061c97e6d00b4e502e910d0b6b
lazeez <qudratov.com26@gmail.com>
2024-09-09 17:36:57 +0500

    adding back-end in proccess

---
+++ src/setupProxy.jsx
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+    app.use(
+        '/http://grscan.uz/swagger/index.html#/partner/post_partner/partner', // Change to your API path
+        createProxyMiddleware({
+            target: 'http://grscan.uz', // Target back-end server
+            changeOrigin: true,
+        })
+    );
+};
